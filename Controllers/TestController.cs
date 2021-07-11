00eb812e05bdab8a544fd76bbf170a34f8a1a945
Inspiron <Inspiron@DESKTOP-IC05KKU>
2021-07-11 12:26:20 +0530

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebAPIEX.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]//http verbs are opitional
+        public string GetString()
+        {
+            return "get string called";
+        }
+        [HttpPost]
+        public string  PostString()
+        {
+            return "post string called";
+        }
+        [HttpPut]
+        public string PutString()
+        {
+            return "Put string called";
+        }
+        [HttpDelete]
+        public string DeleteString()
+        {
+            return "Delete string called";
+        }
+    }
+}
